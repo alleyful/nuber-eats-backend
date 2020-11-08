07ec914004863f9caf8420e26ed9c2844a3faa1a
alleyful <alleyful@naver.com>
2020-11-08 19:26:22 +0900

    4 create account mutation

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
+import { CreateAccountInput } from "./dtos/create-account.dto";
 import { User } from "./entities/user.entity";
 
 @Injectable()
@@ -8,4 +9,19 @@ export class UsersService {
   constructor(
     @InjectRepository(User) private readonly users: Repository<User>
   ) {}
+
+  async createAccount({email, password, role}: CreateAccountInput): Promise<string | undefined> {
+    try {
+      const exists = await this.users.findOne({ email });
+      if(exists) {
+        return "There is a user with email already";
+      }
+      await this.users.save(this.users.create({email, password, role}));
+    } catch(e) {
+      // make error
+      return "Could't create account";
+    }
+    //check new user
+    //create user & hash the password
+  }
 }
