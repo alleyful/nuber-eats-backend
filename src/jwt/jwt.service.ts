abb77f515b67e57f931958d0ceeebdb417ac1e90
alleyful <alleyful@naver.com>
2020-11-08 22:14:47 +0900

    Jwt module and login token

---
+++ src/jwt/jwt.service.ts
@@ -0,0 +1,15 @@
+import { Inject, Injectable } from '@nestjs/common';
+import * as jwt from 'jsonwebtoken';
+import { JwtModuleOptions } from './jwt.interfaces';
+import { CONFIG_OPTIONS } from './jwt.contants';
+
+@Injectable()
+export class JwtService {
+  constructor(
+    @Inject(CONFIG_OPTIONS) private readonly options: JwtModuleOptions,
+  ) {}
+
+  sign(userId: number): string {
+    return jwt.sign({ id: userId }, this.options.privateKey);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,11 +4,13 @@ import { Repository } from "typeorm";
 import { CreateAccountInput } from "./dtos/create-account.dto";
 import { LoginInput } from "./dtos/login.dto";
 import { User } from "./entities/user.entity";
+import { JwtService } from "src/jwt/jwt.service";
 
 @Injectable()
 export class UsersService {
   constructor(
-    @InjectRepository(User) private readonly users: Repository<User>
+    @InjectRepository(User) private readonly users: Repository<User>,
+    private readonly jwtService: JwtService,
   ) {}
 
   async createAccount({email, password, role}: CreateAccountInput): Promise<{ok: boolean; error?: string}> {
@@ -47,9 +49,10 @@ export class UsersService {
         }
       }
 
+      const token = this.jwtService.sign(user.id);
       return {
         ok: true,
-        token: 'lalalal',
+        token,
       }
     } catch (error) {
       return {
