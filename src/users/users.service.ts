389515040d3c1ca15af22639be6457a9de4a282d
alleyful <alleyful@naver.com>
2020-11-08 20:58:56 +0900

    login

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { CreateAccountInput } from "./dtos/create-account.dto";
+import { LoginInput } from "./dtos/login.dto";
 import { User } from "./entities/user.entity";
 
 @Injectable()
@@ -10,18 +11,51 @@ export class UsersService {
     @InjectRepository(User) private readonly users: Repository<User>
   ) {}
 
-  async createAccount({email, password, role}: CreateAccountInput): Promise<[boolean, string?]> {
+  async createAccount({email, password, role}: CreateAccountInput): Promise<{ok: boolean; error?: string}> {
     try {
       const exists = await this.users.findOne({ email });
-      if(exists) {
-        return [false, "There is a user with email already"];
+      if (exists) {
+        return { ok: false, error: 'There is a user with that email already' };
       }
-      await this.users.save(this.users.create({email, password, role}));
-      return [true];
-    } catch(e) {
-      return [false, "Could't create account"];
+      await this.users.save(this.users.create({ email, password, role }));
+      return { ok: true };
+    } catch (e) {
+      return { ok: false, error: "Couldn't create account" };
     }
     //check new user
     //create user & hash the password
+  }
+
+  async login({email, password}: LoginInput): Promise<{ok: boolean; error?: string; token?: string}> {
+    // find the user with the email
+    // check if the password is correct
+    // make a JWT and give it to the User
+
+    try {
+      const user = await this.users.findOne({ email });
+      if(!user) {
+        return {
+          ok: false,
+          error: 'User not found'
+        };
+      }
+      const passwordCorrect = await user.checkPassword(password);
+      if(!passwordCorrect) {
+        return {
+          ok: false,
+          error: 'wrong password',
+        }
+      }
+
+      return {
+        ok: true,
+        token: 'lalalal',
+      }
+    } catch (error) {
+      return {
+        ok: false,
+        error,
+      }
+    }
   }
 }
