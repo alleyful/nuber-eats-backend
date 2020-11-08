83467f0c89563dee456d94a7783c402a73e3a6e0
alleyful <alleyful@naver.com>
2020-11-08 19:33:14 +0900

    4 alternative error

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -10,16 +10,16 @@ export class UsersService {
     @InjectRepository(User) private readonly users: Repository<User>
   ) {}
 
-  async createAccount({email, password, role}: CreateAccountInput): Promise<string | undefined> {
+  async createAccount({email, password, role}: CreateAccountInput): Promise<[boolean, string?]> {
     try {
       const exists = await this.users.findOne({ email });
       if(exists) {
-        return "There is a user with email already";
+        return [false, "There is a user with email already"];
       }
       await this.users.save(this.users.create({email, password, role}));
+      return [true];
     } catch(e) {
-      // make error
-      return "Could't create account";
+      return [false, "Could't create account"];
     }
     //check new user
     //create user & hash the password
